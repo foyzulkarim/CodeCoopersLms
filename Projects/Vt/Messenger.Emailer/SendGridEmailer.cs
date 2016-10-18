915cae2044c1ff5d8598b71b9fff2df756c5e200
Foyzul Karim <foyzulkarim@gmail.com>
2016-10-18 09:59:34 +0600

    commented send grid code to solve build error (for now)

--- Projects/Vt/Messenger.Emailer/SendGridEmailer.cs
+++ Projects/Vt/Messenger.Emailer/SendGridEmailer.cs
@@ -11,30 +11,30 @@ namespace Messenger.Emailer
      
         public async Task<bool> SendEmailAsync(Message message, NetworkCredential credentials)
         {
-            SendGridMessage sendgrid = new SendGridMessage
-            {
-                From = new MailAddress(message.SenderEmail, message.SenderName),
-                Subject = message.Subject,
-                Html = message.Html,
-                Text = message.Text,
-                ReplyTo = new[] {new MailAddress(message.ReplyTo, message.ReplyToName)}
-            };
-            sendgrid.EnableClickTracking(true);
-            if (message.Recipents.Count>1)
-            {
-                sendgrid.AddTo(message.SenderEmail);
-                foreach (string recipent in message.Recipents)
-                {
-                    sendgrid.AddBcc(recipent);                    
-                }
-            }
-            else
-            {
-                sendgrid.AddTo(message.Recipents);                
-            }
+            //SendGridMessage sendgrid = new SendGridMessage
+            //{
+            //    From = new MailAddress(message.SenderEmail, message.SenderName),
+            //    Subject = message.Subject,
+            //    Html = message.Html,
+            //    Text = message.Text,
+            //    ReplyTo = new[] {new MailAddress(message.ReplyTo, message.ReplyToName)}
+            //};
+            //sendgrid.EnableClickTracking(true);
+            //if (message.Recipents.Count>1)
+            //{
+            //    sendgrid.AddTo(message.SenderEmail);
+            //    foreach (string recipent in message.Recipents)
+            //    {
+            //        sendgrid.AddBcc(recipent);                    
+            //    }
+            //}
+            //else
+            //{
+            //    sendgrid.AddTo(message.Recipents);                
+            //}
             
-            Web transportWeb = new Web(credentials);
-            await transportWeb.DeliverAsync(sendgrid);
+            //Web transportWeb = new Web(credentials);
+            //await transportWeb.DeliverAsync(sendgrid);
             return true;
         }
 
