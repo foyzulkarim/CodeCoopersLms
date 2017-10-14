1dbbf1faad39cca17f86ab0426c2ed0e0a99d748
Foyzul Karim <foyzulkarim@gmail.com>
2017-10-14 22:28:07 +0600

    added a web api project (with authentication option)

---
+++ LBL/Lbl/Lbl.Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Lbl.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LBL/Lbl/Lbl.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lbl.Server.WebApiApplication" Language="C#" %>
