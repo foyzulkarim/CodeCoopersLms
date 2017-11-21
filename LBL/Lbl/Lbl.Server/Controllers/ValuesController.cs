9eef93fe0beda63435b5810b33a71055ac2d21c3
zayed-04 <eng.zayed04@gmail.com>
2017-11-21 23:14:18 +0600

    added hiphen

--- LBL/Lbl/Lbl.Server/Controllers/ValuesController.cs
+++ LBL/Lbl/Lbl.Server/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace Lbl.Server.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1-", "value2" };
         }
 
         // GET api/values/5
