f3bddb3ffa272c678b0cdbaeccf0aae2e11f97b2
Huque <Amanul.Huque@yrbrands.com>
2017-11-22 17:09:04 +0600

    Foreign key added to new models

--- LBL/Lbl/Lbl.Model/Content.cs
+++ LBL/Lbl/Lbl.Model/Content.cs
@@ -6,22 +6,42 @@ using System.Threading.Tasks;
 
 namespace Lbl.Model
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     class Content : Entity
     {
+        [Required]
         public int Serial { get; set; }
 
+        [Index]
+        [MaxLength(128)]
         public string CourseId { get; set; }
 
+        [ForeignKey("CourseId")]
+        public virtual Course Course { get; set; }
+
+        [Index]
+        [Required]
+        [MaxLength(250)]
         public string Title { get; set; }
 
+        [Required]
+        [MaxLength(500)]
         public string Description { get; set; }
 
+        [Required]
+        [MaxLength(250)]
         public string Url { get; set; }
 
+        [Required]
         public int TotalSeconds { get; set; }
 
+        [Required]
+        [MaxLength(250)]
         public string Tags { get; set; }
 
+        [Required]
         public int Category { get; set; }
     }
 }
--- LBL/Lbl/Lbl.Model/StudentContent.cs
+++ LBL/Lbl/Lbl.Model/StudentContent.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace Lbl.Model
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     class StudentContent : Entity
     {
+        [Index]
+        [MaxLength(128)]
         public string StudentId { get; set; }
 
+        [ForeignKey("StudentId")]
+        public virtual Student Student { get; set; }
+
+        [Index]
+        [MaxLength(128)]
         public string ContentId { get; set; }
 
-        public int WatchedSeconds { get; set; }
+        [ForeignKey("ContentId")]
+        public virtual Content Content { get; set; }
+
+        public int? WatchedSeconds { get; set; }
 
     }
 }
